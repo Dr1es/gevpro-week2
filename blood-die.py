735c6bb10e4967603af7a4bb6e2e7bd64d65d125
Dr1es <richard_neyhoff@hotmail.com>
2015-03-08 22:27:31 +0100

    JSON opdracht

--- blood-die.py
+++ blood-die.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python
 
 import json
+from collections import namedtuple
 
+json_data = open("blood-die.json")
+list = json.load(json_data)
 
+newlist = [elem for elem in list if elem[2].split()[0] == elem[3].split()[0]]
+gelijkwoord = namedtuple("gelijkwoord", "language, word")
+
+landlist = []
+for i in newlist:
+    languages = gelijkwoord(i[0], i[2])
+    landlist.append(languages)
+print(landlist)
